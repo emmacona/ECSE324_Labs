5688420fb212c42e7b4d94d877244c0f00de01ef
emmacona <emmanuelle.coutu-nadeau@mail.mcgill.ca>
2020-02-07 14:04:42 -0500

    C prog done

--- Lab_2/max.c
+++ Lab_2/max.c
@@ -1,1 +1,16 @@
 // max using C
+
+#include <stdio.h>
+
+int main(){
+	int a[5] = {1, 20, 3, 4, 5};
+	int max_val = 0;
+
+	for(int i = 0; i < 5; i++){
+		if(max_val < a[i]){
+			max_val = a[i];
+		}
+	}
+
+	return(max_val);
+}
